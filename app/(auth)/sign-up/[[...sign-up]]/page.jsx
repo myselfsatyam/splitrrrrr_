bb6788cd9aa28a44115e45a87f002dc55939e0ac
myselfsatyam <satyamsharma21589@gmail.comm>
2025-05-15 03:48:16 +0530

    Preparing for deployment

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,9 @@
+// app/sign-up/[[...sign-up]]/page.tsx
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return <SignUp />;
+};
+
+export default SignUpPage;
